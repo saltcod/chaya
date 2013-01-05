e134efe010494520459ff09bf66e773e0d7184e4
Terry Sutton <saltcod@gmail.com>
2013-01-05 13:30:25 -0330

    Paul irish's solution neither for html classes

--- header.php
+++ header.php
@@ -8,7 +8,14 @@
  * @since waterstreet 1.0
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+
+<!--[if lt IE 7]>  <html class="ie ie6 lte9 lte8 lte7"> <![endif]-->
+<!--[if IE 7]>     <html class="ie ie7 lte9 lte8 lte7"> <![endif]-->
+<!--[if IE 8]>     <html class="ie ie8 lte9 lte8"> <![endif]-->
+<!--[if IE 9]>     <html class="ie ie9 lte9"> <![endif]-->
+<!--[if gt IE 9]>  <html> <![endif]-->
+<!--[if !IE]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+ 
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
