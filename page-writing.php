d18b97b1cf8f85cf778fa30309b9f0c6001b4a27
Terry Sutton <saltcod@gmail.com>
2013-04-20 09:43:55 -0230

    Add writing page

---
+++ page-writing.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* The main template file.
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+* Learn more: http://codex.wordpress.org/Template_Hierarchy
+*
+* @package waterstreet
+* @since waterstreet 1.0
+*/
+
+get_header(); ?>
+
+<section id="primary" class="content-area">
+    <div class="wrap group">
+        <div id="content" class="site-content" role="main">
+           
+
+
+
+
+        </div><!-- #content .site-content -->
+    </div><!-- .wrap -->
+</div><!-- #primary .content-area -->
+
+<?php get_footer(); ?>
+
+
