f34838d00a15ea5413f6eec2154677b83eb2901b
Terry Sutton <saltcod@gmail.com>
2013-04-20 09:46:15 -0230

    Milestones content is now handled by WP

--- page-milestones.php
+++ page-milestones.php
@@ -1,73 +1,133 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package waterstreet
- * @since waterstreet 1.0
- */
 
-get_header(); ?>
+<div class="milestone-list">
+	<div class="milestone">
+		<h3 class="title">Lucern Festival Artist in Residence. A wide retrospective of the composer's work, including the premier of 2 orchestral pieces: At the fringe of our gaze (21') for WEDO and Daniel Barneboim White Wind Waiting for Stephan Schmidt (guitar) and SWR Baden-Baden with François-Xavier Roth conducting </h3>
+		<date>2013</date>
+	</div>
 
-<section id="primary" class="content-area">
-	<div class="wrap group">
-		
-		
-		<div id="content" class="site-content six" role="main">
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="milestone">
+		<h3 class="title"> Miller theater concert portrait with Either Or conducted by Rick Carrick and David Shively.</h3>
+		<date>2011</date>
+	</div>
 
- 				<div class="entry-content">
-					
-					<p>Proin id felis ut dui posuere adipiscing sit amet et arcu. Nulla malesuada auctor nisi, a gravida felis blandit in. In convallis, dui vel ultrices ultricies, erat magna sollicitudin sem, at cursus ligula nibh et erat. Integer orci augue, sollicitudin non vestibulum sed, auctor a ligula. Morbi congue porta leo, eget posuere mauris vulputate vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+	<div class="milestone">
+		<h3 class="title">The quiet commissioned by Bayerischer Rundfunk orchetsra with Brad Lubman conducting,(Germany) 2011 Zohar Iver, commissioned by Culturespace for Bern Philharmonic Orchestra and Ensemble Nikel (Switzerland) and 2012 Esh- Commissioned by the Philharmonic Orchestra of Cottbus (Germany) Three orchestral pieces marking a new direction in Czernowin's output. </h3>
+		<date>2010</date>
+	</div>
 
-					<img src="http://chayaczernowin.com/wp-content/themes/chaya/images/field-landscape.jpg" alt="">
+	<div class="milestone">
+		<h3 class="title">Moved back to the US to undertake professorship at Harvard </h3>
+		<date>2009</date>
+	</div>
 
+	<div class="milestone">
+		<h3 class="title"> The Dialogue experiment, a collaborative piece written for Donaueschingen Festival by Czernowin and 6 of her former studnets: Rick Burkhardt, Peter Edwards, Michelle Lou, Chris Mercer, Ming Tsao and Rob Wannamaker. The piece was performed by Ascolta and conducted by Jonathan Stockhammer. </h3>
+		<date>2008</date>
+	</div>
 
-					<div class="milestone-list">
-						
-						<div class="milestone">
-							<h3 class="title">Composition of ballet music Maa, premiered by Finnish National Ballet</h3>
-							<date>Jan 1, 2003</date>
-						</div>
+	<div class="milestone">
+		<h3 class="title"> The Triptych Maim 2002- 2007 for Orchestra Electronics and Soloists (long term collaborators Rico Gubler, Peter Veale, Seth Josel, Mary Oliver and John Mark Harris) Dur: 50 minutes, performed in its entirety in Maemusik 2007 in a concert celebrating the 50th birthday of the composer, with the soloists, Konzerthause orchetsra, and the Experimental Studio, conducted by Johannes Kalitzke. </h3>
+		<date>2007</date>
+	</div>
 
-						
-						<div class="milestone">
-							<h3 class="title">Graal Théâtre</h3>
-							<date>2002</date>
-						</div>
-						
-						<div class="milestone">
-							<h3 class="title">Composition of ballet music Maa, premiered by Finnish National Ballet</h3>
-							<date>2000</date>
-						</div>
-						
-						<div class="milestone">
-							<h3 class="title">L'Amour de Loin</h3>
-							<date>1998</date>
-						</div>
-						
-						<div class="milestone">
-							<h3 class="title">Ars Electronica Prize for Stilleben and Io; one year residency at the University of San Diego</h3>
-							<date>1995</date>
-						</div>
-					</div>
+	<div class="milestone">
+		<h3 class="title">Moved with her family to Vienna to take a position as a composition professor at the University of Music and Performing Arts, Vienna</h3>
+		<date>2006</date>
+	</div>
 
+	<div class="milestone">
+		<h3 class="title"> Zaide Adama, fragments. Adama is a counterpoint piece to Zaide, by Mozart which is written to be performed between and at time simultaneously with Zaide. Commissioned by Salzburg festival. This was the second collaboration with Claus Guth and christian schmidt.</h3>
+		<date>2006</date>
+	</div>
 
- 
-				</div><!-- .entry-content -->
+	<div class="milestone">
+		<h3 class="title"> Wintersongs series, commissioned by Ircam and Intercontemporain; Ensemble Modern; and Rockefeller foundation for Sospeso. This marks the first time Czernowin employed the idea of a series as a compositional principle, an engagement which has since continued. </h3>
+		<date>2003/4</date>
+	</div>
 
+	<div class="milestone">
+		<h3 class="title"> Pnima ... Ins Innere a chamber opera commissioned by the city of Munich for the Munich Biennial, where Czernowin was a featured composer.This was the first collaboration with director Claus Guth and stage designer Christian Schmidt. The opera will have this year its 4th production. </h3>
+		<date>2000</date>
+	</div>
 
+	<div class="milestone">
+		<h3 class="title"> Shu Hai Practices Javelin for one voice and many recorded voices a commission of Eclat festival, performed by Ute Wassermann and the Experimental Studio. This piece is the first collaboration of Czernowin with the experimental studio, a meaningful collaboration for the composer, which continues till today. The collaborative element here is present also in the work with Ute Wassermann who figured out the vocal material for the piece together with the composer . </h3>
+		<date>1997</date>
+	</div>
 
-			</article><!-- #post-<?php the_ID(); ?> -->
+	<div class="milestone">
+		<h3 class="title"> UCSD - Czernowin gets a position as an assistant professor at the University of California San Diego. </h3>
+		<date>1997</date>
+	</div>
 
+	<div class="milestone">
+		<h3 class="title"> AFATSIM (gall nuts) first paid commission for musikprotokoll im steirischen herbst, Graz Austria for Ensemble Recherche</h3>
+		<date>1996</date>
+	</div>
 
-		</div><!-- #content .site-content -->
-	</div><!-- .wrap -->
-</div><!-- #primary .content-area -->
+	<div class="milestone">
+		<h3 class="title">Fellow Akademie Schloss Solitude, Stuttgart, the family moves to Germany. </h3>
+		<date>1996-7</date>
+	</div>
 
-<?php get_footer(); ?>
+	<div class="milestone">
+		<h3 class="title"> Became a mother </h3>
+		<date>1995</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> String quartet played by the Arditti quartet in Israel and At the Darmstadt Summer courses. </h3>
+		<date>1995</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> Die Kreuzung, for U Saxophone and Bass, recorded by Mayumi Miyata, Takashi Saito & Keizo Misoiri</h3>
+		<date>1994</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title">Living in Tokyo (Koganei Shi), Japan on an Asahi shimbum fellowship and a USA NEA grant. </h3>
+		<date>1993-1995</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> Dam Sheon Hachol (the hour glass bleeds still) for string sextet Kranichsteiner musikpreis in Darmstadt summer courses. </h3>
+		<date>1992</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> INA for bass Flute and recorded 6 other flutes MANOALCHADIA, for flute and 2 femel voices. Most pieces written before these 2 pieces (79-87) were withdrawn.</h3>
+		<date>1988</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> Czernowin moves to La Jolla to Attend the University of Claifronia San Diego. Here she experiences free improvisation ( Mary Oliver, Ute Wassermann and KIVA) and here she meets her future husband composer Steven Kazuo Takasugi</h3>
+		<date>1987</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> The piece BIRDS is played by the Deutsche Junge Philharmonie in Israel in the presence of The Israeli prime minister and The German president. </h3>
+		<date>1985</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> Belin DAAD, first meeting with the music of Scelsi </h3>
+		<date>1983</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> CUBES for orchetsra divided to 3 ensembles and singer is played in the frame work of young artist week. Beersheva Sinfonietta and Lisa Levine prefromed. </h3>
+		<date>1980</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title"> Kazuo Ono, one of the father of Butoh dance visits Israel.</h3>
+		<date>1979</date>
+	</div>
+
+	<div class="milestone">
+		<h3 class="title">Played in the progressive rock group "Etz" (tree) with Arnon Palty (bass) , Michael Meltzer (flute) and Nir Boras (keyboard and singer) as a vocalist songwriter and pianist and also in "Pandora's box" with Eldad Arnon (keyboard) , Yaki kop (percussion) and with Bassist Arnon Palty who became one of the best Jazz composers/players in Israel. </h3>
+		<date>1975</date>
+	</div>
+	
+</div> <!-- .milestone-list -->
