685fef0c457082c7a5b0c868b0622f5212a8bbe1
Terry Sutton <saltcod@gmail.com>
2013-04-20 09:45:40 -0230

    Remove "Posted on…."

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -123,7 +123,7 @@ if ( ! function_exists( 'waterstreet_posted_on' ) ) :
  * @since waterstreet 1.0
  */
 function waterstreet_posted_on() {
-	printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'waterstreet' ),
+	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'waterstreet' ),
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
