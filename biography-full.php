5d2bf8299d548f8ef0d21fd50421b54369a70dc3
Terry Sutton <saltcod@gmail.com>
2013-05-02 11:36:23 -0230

    Remove bottom margin from blockquote > p

---
+++ biography-full.php
@@ -0,0 +1,57 @@
+<div class="block group">
+	<div class="two-col">
+		<p>Chaya Czernowin was born and brought up in Israel. After her studies in Israel, at the age of 25, she continued studying in Germany (DAAD grant), the US and then was invited to live in Japan (Asahi Shimbun Fellowship and American NEA grant) Tokyo, in Germany (at the Akademie Schloss Solitude) and in Vienna. Her music has been performed throughout the world, and she has held professorships at UCSD, and was the first woman to be appointed as a composition professor at the University of Music and Performing Arts in Vienna, Austria (2005-2009), and at Harvard University in (2009 and on) where she is the Walter Bigelow Rosen Professor of Music . Together with her husband, composer Steven Kazuo Takasugi, she has founded the summer Academy at Schloss Solitude, a biannual course for composers.</p>
+	</div>
+
+	<blockquote>
+		"Vital, visceral, wild and undefined as experience itself - can music be that? I have heard such music, rarely, but, it has changed my life. Attempting to work towards it, though, is a difficult balancing act: one must be as sensually sensitive as if one has no skin, while exercising the analytical clarity, precision and focus of holding a surgeon's knife."
+		<cite>—Chaya Czernowin</cite>
+	</blockquote>
+
+	<div class="two-col">	
+		<p>Czernowin’s output includes chamber and orchestral music, with and without electronics. Her works were commissioned by and played in many of the most significant new music festival in Europe and also in Japan Korea, Australia US and Canada. She composed 2 large scale works for the stage: Pnima...ins Innere (2000, commission of the Munich Biennale) chosen to be the best premiere of the year by Opernwelt yearly critic survey, and Adama (2004/5) with Mozart's Zaide (commission of the Salzburg Festival 2006). Both operas have biographical and political undercurrents. She was appointed Artist in residence at the Salzburg Festival in 2005/6 and will be Artist in residence at the Lucern Festival, Switzerland in 2013. Characteristic of her work are working with metaphor as a means of reaching a sound world which is unfamiliar; the use of noise and physical parameters as weight, textural surface (as in smoothness or roughness etc), problematiszation of time and unfolding and shifting of scale in order to create a vital, visceral and direct sonic experience. </p>
+	</div>  
+</div> <!-- .block group -->
+
+<hr>
+
+<p>In addition to numerous other prizes, Czernowin represented Israel at Uncesco composer's Rostorum 1980; and was awarded the DAAD scholarship 83- 85; Stipendiumpreis (88) and Kranichsteiner Musikpreis (92), at the Darmstadt Fereinkurse,: IRCAM (Paris) reading panel commission 1998; scholarships of SWR experimental Studio Freiburg 98/00/ 01; The composer’s prize of Siemens Foundation 2003; the Rockefeller Foundation, 2004; a nomination as a fellow to the Wissenschaftkolleg Berlin in 2008; Fromm Foundation Award 2009; and Guggenheim Foundation fellowship 2011. She is published by Schott. Her music is recorded on Mode records NY, Wergo, Col Legno, Deutsche Gramophone, Neos , Ethos, Telos and Einstein Records. She lives near Boston with, composer Steven Kazuo Takasugi and their son. </p>
+
+
+
+<div class="block group">
+	
+	<h2>Attempting to touch sound</h2>
+	
+	<div class="two-col">
+		<p>Gazing upon a landscape that is at times delicately fragile, taking notice of rocks, grains of sand,	grass and patterns of light, there is an underlying corrosion; a glacial movement of a scale that seems imperceptible at first, but it is growing, teetering on the edge of transformation. It is the force which comes just before a landslide. Chaya Czernowin's music possesses an almost primordial gaze, originating from an immense and bottomless core. There is a physical immediacy to her work, and while sensing an organic familiarness (like leaves, earth, wind, water, creatures, body, stomach), this is also a very foreign place - intricately constructed, shaped, and precise like an object carefully invented by a skilled hand. Her work resides in a place where experience is felt as a kind of instinctive embodied awareness that precedes language; where words have yet to be fully formed. For Czernowin, communication begins underneath, beyond categories of musical style, motives or metered rhythms. Meaning is found as an accretion; it is emergent and fluid as it attempts to reveal the tenebrous substance beneath.</p>	
+
+		<p>This process of "uncovering" is a central concept to Czernowin's oeuvre. Having lived in five countries since the age of 25, the question of identity forged the impulse to excavate the layers that cohere a selfness to reveal something more coextensive. She writes:</p>
+	</div>
+
+
+	<blockquote>"...my music almost obsessively tried to stretch the idea of identity: from the inside, exploring separate and contrasting voices (or identities) within one larger identity, investigating how much dissent and difference can exist before the seams start to tear apart and all of a sudden, we have more than one identity. Dialectically, I stretched identity by combining different instruments into a unified meta instrument ." 
+		<cite>- The Last Tiger </cite>
+	</blockquote>
+
+
+	<p>The use of metaphor is also central to Czernowin’s compositional thought. Because words have no credence and definitions are therefore slippery, only metaphors are suitable tools to get closer to the thrust of her intentions. </p>
+	
+	<hr>
+	
+	<div class="two-col">
+		<p>In the massive work, Maim (water), Czernowin sought to use of the image of water. Maim is scored for orchestra, five soloists (a disparate instrumental formation of tubax, guitar, harpsichord, oboe and viola) and live electronics. It appears in three large movements written from 2001-2006, Maim zarim maim Gnuvim (strange water stolen water), The memory of water, and Mei mecha'a (water of dissent). What was intended as an engagement with the "beauty and fluidity of water" became disrupted by the events of September 2001. Water hardened and for her, "the piece became a stage for the dialogue which emerges between the necessity to turn the eyes inside and shut the world out and the necessity of being aware and reacting to a troubled reality." It is at this point that she understands and embraces the idea of the political in her work.</p>
+		<br><br>
+		<p>Along with her body of chamber and orchestral pieces are two operas. Her first, Pnima…ins Innere, for the Munich Biennale (2000) was based loosely on David Grossmann's novel, "See under: love." It is a haunting work that portrays the difficulty of communicating a traumatic experience between a boy and his grandfather. The weight is so oppressive, burdensome, that words have retreated, reverting to utterances. The work was awarded the Bavarian Theater Prize and was named “Best Premiere of the Year” by the critic's survey of the magazine Opernwelt. In 2005 and in 2006 Czernowin was composer in residence at the Salzburg Festival, where she was commissioned to write a counterpoint piece to Mozart's fragment, “Zaïde.” The resulting work Zaide/Adama, is a first attempt of its kind to answer an unfinished work with an intervening contemporary “counterpoint work.” With two orchestras and two casts, Czernowin’s Adama is a portrayal of forbidden love between a Palestinian and a Jew. </p>
+		
+	</div>
+	
+	<hr>
+	
+	<p>Physicality. In the large orchestral works, The Quiet (2011), Zohat Iver (blind radiance) (2011) and Esh (2011), Czernowin further extends the experiential potentialities to transcend language in attempting towards music "to be seen, touched and felt through the ear." The threads of connection can be very fragile, holding together rather heavy, weighted sections of material, along with multiple strands of opposing forces. They are following the laws of nature (in an invented landscape), of energy, entropy, and transference. In fact, her music is concerned with the physics: matter, energy, atoms, particles and their interactions and motion over time that is variously fragmented, altered, static or abrupt. She does not prescribe the listening experience, but rather lays the foundation for personal observation. It is this balance of addressing both the sensing body and the grasping mind in her music that motivates her; a balance that is on the edge of tipping, seeking a third realm, one that is on the fringe of our consciousness; a liminal space that "digests" personal and shared histories; mythologies worked out by dreams towards something that is in between, defying reduction by category or description; it is always in motion, alive. </p>
+
+	<cite>-Michelle Lou</cite>
+
+
+</div>
+ 
